297547fab3b9dddd5e994171946801495c062b4c
unknown <egkirpichev@gmail.com>
2022-07-26 01:25:41 +0300

    refactor(tips-calculator): move App component styles to a separate file

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
-import styled from "styled-components";
+import {Background, Wrapper} from "./styles"
 import { Form } from "./components/Form/Form";
-import { Color } from "./types";
 
 const App = () => {
   return (
@@ -11,44 +10,5 @@ const App = () => {
 		</Background>
  );
 }
-
-const Wrapper = styled.div`
-	max-width: 1512px;
-	height: 100vh;
-	margin: 0px auto;
-	padding-top: 203px;
-	background: ${Color.Wrapper};
-	overflow: hidden;
-
-	&::before {
-		content: "";
-		position: absolute;
-		transform: translate(-44px, -299px);
-		display: block;
-		width: 250px;
-		height: 250px;
-		background: ${Color.Green};
-		border-radius: 125px;
-		opacity: 0.31;
-	}
-
-	&::after {
-		content: "";
-		position: absolute;
-		transform: translate(-108px,-878px);
-		display: block;
-		width: 250px;
-		height: 250px;
-		background: ${Color.Green};
-		border-radius: 125px;
-		opacity: 0.31;
-		overflow: hidden;
-	}
-
-`
-
-const Background = styled.div`
-	background: ${Color.Backround};
-`
 
 export default App;
